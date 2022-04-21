ca65e363c7466d239be168fd3fd2c418851b3de8
simonsasse <simonsasse97@gmail.com>
2022-04-21 18:11:25 +0200

    add screen dummies etc

---
+++ lib/screens/auth/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/main/start_game_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StartGameScreen extends StatefulWidget {
+  const StartGameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StartGameScreen> createState() => _StartGameScreenState();
+}
+
+class _StartGameScreenState extends State<StartGameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("StartGame not implemented.")),
+    );
+  }
+}
