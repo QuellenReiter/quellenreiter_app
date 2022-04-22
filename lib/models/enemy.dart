44c1bdd435b99197ed7cb8f9b3bb0e9565ce8d00
simonsasse <simonsasse97@gmail.com>
2022-04-22 12:34:01 +0200

    navigation basics done and running

--- lib/models/enemy.dart
+++ lib/models/enemy.dart
@@ -2,6 +2,8 @@ class Enemy {
   late final String name;
   late final String userID;
   late final String emoji;
+  late final bool notAcceptedByOther;
+  late final bool notAcceptedByPlayer;
 }
 
 class Enemies {
