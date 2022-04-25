ac8001d4573d74e266d792cee6050159df241a34
simonsasse <simonsasse97@gmail.com>
2022-04-25 13:47:05 +0200

    sketched settings screen

--- lib/screens/main/settings_screen.dart
+++ lib/screens/main/settings_screen.dart
@@ -10,8 +10,32 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Settings not implemented.")),
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: ListView(
+        children: [
+          ElevatedButton.icon(
+            onPressed: () => {},
+            icon: const Icon(Icons.emoji_emotions),
+            label: const Text("Emoji auswählen"),
+          ),
+          ElevatedButton.icon(
+            onPressed: () => {},
+            icon: const Icon(Icons.switch_access_shortcut),
+            label: const Text("Namen ändern."),
+          ),
+          ElevatedButton.icon(
+            onPressed: () => {},
+            icon: const Icon(Icons.logout),
+            label: const Text("Abmelden"),
+          ),
+          ElevatedButton.icon(
+            onPressed: () => {},
+            icon: const Icon(Icons.delete_forever),
+            label: const Text("Account löschen"),
+          ),
+        ],
+      ),
     );
   }
 }
