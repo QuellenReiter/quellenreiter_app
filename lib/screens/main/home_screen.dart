d539d4845db849f72852967a18d5dca5a135b405
simonsasse <simonsasse97@gmail.com>
2022-04-25 11:57:14 +0200

    add bottom navigation bar

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({
+    Key? key,
+    required this.index,
+    required this.bottomNavCallback,
+    required this.body,
+    required this.title,
+  }) : super(key: key);
 
+  final int index;
+  final Function(int) bottomNavCallback;
+  final Widget body;
+  final String title;
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -11,16 +21,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.rocket_launch),
+            icon: Icon(Icons.rocket),
+            label: 'Büro',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle_rounded),
+            label: 'Freund:innen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.archive),
+            label: 'Archiv',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Einstellungen',
+          ),
+        ],
+        currentIndex: widget.index,
+        onTap: widget.bottomNavCallback,
+      ),
       appBar: AppBar(
-        title: Text("Dein Büro"),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Home not implemented."),
-          ],
-        ),
-      ),
+      body: widget.body,
     );
   }
 }
