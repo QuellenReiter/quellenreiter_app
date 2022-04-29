9172c39ebfff6cc40aca404fdb732cb1268ddb20
simonsasse <simonsasse97@gmail.com>
2022-04-29 13:23:54 +0200

    start new game and open game workflow implemented

--- lib/screens/main/start_game_screen.dart
+++ lib/screens/main/start_game_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:quellenreiter_app/models/enemy.dart';
+import 'package:quellenreiter_app/models/quellenreiter_app_state.dart';
+
+import '../../constants/constants.dart';
+import '../../widgets/enemy_card.dart';
 
 class StartGameScreen extends StatefulWidget {
-  const StartGameScreen({Key? key}) : super(key: key);
-
+  const StartGameScreen({Key? key, required this.appState}) : super(key: key);
+  final QuellenreiterAppState appState;
   @override
   State<StartGameScreen> createState() => _StartGameScreenState();
 }
@@ -10,11 +15,41 @@ class StartGameScreen extends StatefulWidget {
 class _StartGameScreenState extends State<StartGameScreen> {
   @override
   Widget build(BuildContext context) {
+    List<Enemy> enemiesWithNoGame = widget.appState.player!.friends!.enemies
+        .where((e) => e.openGame == null)
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Neues Spiel"),
       ),
-      body: Center(child: Text("list of all friends.")),
+      body: Column(
+        children: [
+          if (enemiesWithNoGame.isNotEmpty)
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: ListView.builder(
+                  itemCount: enemiesWithNoGame.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return EnemyCard(
+                      enemy: enemiesWithNoGame[index],
+                      onTapped: (enemy) => {},
+                    );
+                  },
+                ),
+              ),
+            )
+          else
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () =>
+                    widget.appState.handleNavigationChange(Routes.friends),
+                icon: const Icon(Icons.gamepad_outlined),
+                label: const Text("Freund:innen einladen"),
+              ),
+            )
+        ],
+      ),
     );
   }
 }
