16b0eccd23989f19a59f9b6e7d574a13f2049e54
simonsasse <simonsasse97@gmail.com>
2022-05-05 10:58:03 +0200

    add autofill hints

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quellenreiter_app/constants/constants.dart';
 import 'package:quellenreiter_app/models/quellenreiter_app_state.dart';
 
@@ -37,71 +38,88 @@ class _LoginScreenState extends State<LoginScreen> {
         title: Text("Login"),
       ),
       body: Center(
-          child: Column(children: [
-        Flexible(
-          child: ValueListenableBuilder(
-            valueListenable: usernameController,
-            builder: (context, TextEditingValue value, __) {
-              return Container(
-                padding: const EdgeInsets.all(5),
-                child: TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                    labelText: "Nutzername",
-                    border: const OutlineInputBorder(),
-                    errorText: Utils.checkIfEmpty(usernameController),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-        Flexible(
-          child: ValueListenableBuilder(
-            valueListenable: passwordController,
-            builder: (context, TextEditingValue value, __) {
-              return Container(
-                padding: const EdgeInsets.all(5),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: "Passwort",
-                    border: const OutlineInputBorder(),
-                    errorText: Utils.checkIfEmpty(passwordController),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-        widget.appState.error == null
-            ? const SizedBox.shrink()
-            : Container(
-                padding: const EdgeInsets.all(10),
-                color: Colors.red,
-                child: SelectableText(
-                  widget.appState.error!,
-                  style: const TextStyle(
-                    fontSize: 20,
-                  ),
+        child: Column(
+          children: [
+            Flexible(
+              child: AutofillGroup(
+                child: Column(
+                  children: [
+                    Flexible(
+                      child: ValueListenableBuilder(
+                        valueListenable: usernameController,
+                        builder: (context, TextEditingValue value, __) {
+                          return Container(
+                            padding: const EdgeInsets.all(5),
+                            child: TextField(
+                              controller: usernameController,
+                              decoration: InputDecoration(
+                                labelText: "Nutzername",
+                                border: const OutlineInputBorder(),
+                                errorText:
+                                    Utils.checkIfEmpty(usernameController),
+                              ),
+                              autofillHints: const [AutofillHints.username],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Flexible(
+                      child: ValueListenableBuilder(
+                        valueListenable: passwordController,
+                        builder: (context, TextEditingValue value, __) {
+                          return Container(
+                            padding: const EdgeInsets.all(5),
+                            child: TextField(
+                              obscureText: true,
+                              controller: passwordController,
+                              decoration: InputDecoration(
+                                labelText: "Passwort",
+                                border: const OutlineInputBorder(),
+                                errorText:
+                                    Utils.checkIfEmpty(passwordController),
+                              ),
+                              autofillHints: const [AutofillHints.password],
+                              onEditingComplete: () =>
+                                  TextInput.finishAutofillContext(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-        Flexible(
-          child: ElevatedButton.icon(
-            onPressed: () => widget.appState.tryLogin(
-              usernameController.text,
-              passwordController.text,
             ),
-            icon: const Icon(Icons.login),
-            label: const SelectableText("Einloggen"),
-          ),
+            widget.appState.error == null
+                ? const SizedBox.shrink()
+                : Container(
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.red,
+                    child: SelectableText(
+                      widget.appState.error!,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+            Flexible(
+              child: ElevatedButton.icon(
+                onPressed: () => widget.appState.tryLogin(
+                  usernameController.text,
+                  passwordController.text,
+                ),
+                icon: const Icon(Icons.login),
+                label: const SelectableText("Einloggen"),
+              ),
+            ),
+            TextButton(
+              onPressed: () => widget.appState.route = Routes.signUp,
+              child: const Text("Anmelden"),
+            )
+          ],
         ),
-        TextButton(
-          onPressed: () => widget.appState.route = Routes.signUp,
-          child: const Text("Anmelden"),
-        )
-      ])),
+      ),
     );
   }
 }
