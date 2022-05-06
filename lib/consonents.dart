45e1f63df9dc12383ed81ac228938263d3058be8
simonsasse <simonsasse97@gmail.com>
2022-05-06 09:24:59 +0200

    added statement fetching and connection to statement db

--- lib/consonents.dart
+++ lib/consonents.dart
@@ -1,3 +1,8 @@
-String kParseApplicationId = "VlBYTn9J0cp6haOrjObgU6NBW6jU2K70o7P7dnmU";
-String kParseClientKey = "pbhe2o8eQv7hsJkNaBpAhOyElcMRDLfBQbeNdxYT";
-String kUrl = "https://parseapi.back4app.com/graphql";
+String userDatabaseApplicationID = "VlBYTn9J0cp6haOrjObgU6NBW6jU2K70o7P7dnmU";
+String userDatabaseClientKey = "pbhe2o8eQv7hsJkNaBpAhOyElcMRDLfBQbeNdxYT";
+String userDatabaseUrl = "https://parseapi.back4app.com/graphql";
+
+String statementDatabaseApplicationID =
+    "FeP6gb7k9R2K9OztjKWA1DgYhubqhW0yJMyrHbxH";
+String statementDatabaseClientKey = "fuM8AcoXVbDdRstivGRMdSIsnGxT2Zj7tGTbk4Od";
+String statementDatabaseUrl = "https://parseapi.back4app.com/graphql";
