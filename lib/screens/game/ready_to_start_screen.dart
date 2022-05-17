f28d2d3dec0ae5a58f42fb3d5b81c9b7500385f3
simonsasse <simonsasse97@gmail.com>
2022-05-17 13:29:21 +0200

    start game screen implementation

--- lib/screens/game/ready_to_start_screen.dart
+++ lib/screens/game/ready_to_start_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quellenreiter_app/models/enemy.dart';
+import 'package:quellenreiter_app/models/quellenreiter_app_state.dart';
 
 class ReadyToStartScreen extends StatefulWidget {
-  const ReadyToStartScreen({Key? key}) : super(key: key);
-
+  const ReadyToStartScreen({Key? key, required this.appState})
+      : super(key: key);
+  final QuellenreiterAppState appState;
   @override
   State<ReadyToStartScreen> createState() => _ReadyToStartScreenState();
 }
@@ -10,8 +13,82 @@ class ReadyToStartScreen extends StatefulWidget {
 class _ReadyToStartScreenState extends State<ReadyToStartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("readyScreen not implemented.")),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Spielen"),
+      ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // heading
+            Text("Spiel"),
+            // user and enemy
+            Flexible(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(widget.appState.player!.emoji),
+                        Text(widget.appState.player!.name),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(widget.appState.currentEnemy!.emoji),
+                        Text(widget.appState.currentEnemy!.name),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Bottom button:
+            if (widget.appState.currentEnemy == null ||
+                widget.appState.currentEnemy!.openGame == null)
+              const Text("Fehler.")
+            else if (widget.appState.currentEnemy!.openGame!.isPlayersTurn())
+              if (widget.appState.currentEnemy!.openGame!.playerAnswers.isEmpty)
+                Flexible(
+                  child: ElevatedButton(
+                    child: const Text("Spielen"),
+                    onPressed: () {},
+                  ),
+                )
+              else
+                Flexible(
+                  child: ElevatedButton(
+                    child: const Text("Weiter spielen"),
+                    onPressed: () {},
+                  ),
+                )
+            else
+              Flexible(
+                child: ElevatedButton(
+                  child: Text(
+                      "Warten, bis ${widget.appState.currentEnemy!.name} gespielt hat."),
+                  onPressed: null,
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
