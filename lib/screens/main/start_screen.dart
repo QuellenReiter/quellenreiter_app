7ae24b3550683a53ca951533c0debcd45835dbef
simonsasse <simonsasse97@gmail.com>
2022-05-24 17:53:53 +0200

    add graphs

--- lib/screens/main/start_screen.dart
+++ lib/screens/main/start_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:quellenreiter_app/constants/constants.dart';
 import 'package:quellenreiter_app/models/quellenreiter_app_state.dart';
@@ -14,69 +15,162 @@ class StartScreen extends StatefulWidget {
 class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text("Hallo, " +
-                widget.appState.player!.name +
-                " " +
-                widget.appState.player!.emoji),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Text(
+            widget.appState.player!.emoji,
+            style: Theme.of(context).textTheme.headline1,
           ),
-          Padding(
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Text(
+            "Hallo, " + widget.appState.player!.name,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        Flexible(
+          child: Padding(
             padding: const EdgeInsets.all(10),
-            child: Text(
-              "Falsch als Fakt bezeichnet: " +
-                  widget.appState.player!.falseCorrectAnswers.toString(),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: PieChart(
+                          PieChartData(
+                            centerSpaceRadius: double.infinity,
+                            sections: [
+                              PieChartSectionData(
+                                  title: "nicht geglaubt",
+                                  value: widget
+                                      .appState.player!.falseCorrectAnswers
+                                      .toDouble(),
+                                  color: DesignColors.red),
+                              PieChartSectionData(
+                                  title: "richtig",
+                                  value: widget
+                                      .appState.player!.trueCorrectAnswers
+                                      .toDouble(),
+                                  color: DesignColors.green)
+                            ],
+                          ),
+                          swapAnimationDuration:
+                              Duration(milliseconds: 400), // Optional
+                          swapAnimationCurve: Curves.linear,
+                        ),
+                      ),
+                      Text(
+                        "Fakten",
+                        style: Theme.of(context).textTheme.headline5,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                  height: 0,
+                ),
+                Flexible(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: PieChart(
+                          PieChartData(
+                            centerSpaceRadius: double.infinity,
+                            sections: [
+                              PieChartSectionData(
+                                  title: "Reingefallen",
+                                  value: widget
+                                      .appState.player!.falseFakeAnswers
+                                      .toDouble(),
+                                  color: DesignColors.red),
+                              PieChartSectionData(
+                                  title: "entlarvt",
+                                  value: widget.appState.player!.trueFakeAnswers
+                                      .toDouble(),
+                                  color: DesignColors.green)
+                            ],
+                          ),
+                          swapAnimationDuration:
+                              Duration(milliseconds: 150), // Optional
+                          swapAnimationCurve: Curves.linear,
+                        ),
+                      ),
+                      Text(
+                        "Fakes",
+                        style: Theme.of(context).textTheme.headline5,
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              "Falsch als Fake bezeichnet: " +
-                  widget.appState.player!.falseFakeAnswers.toString(),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              "Richtig als Fakt erkannt: " +
-                  widget.appState.player!.trueCorrectAnswers.toString(),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              "Richtig als Fake erkannt: " +
-                  widget.appState.player!.trueFakeAnswers.toString(),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              "Anzahl bewerteter Aussagen: " +
-                  (widget.appState.player!.trueFakeAnswers +
-                          widget.appState.player!.trueCorrectAnswers +
-                          widget.appState.player!.falseCorrectAnswers +
-                          widget.appState.player!.falseFakeAnswers)
-                      .toString(),
-            ),
-          ),
-          ElevatedButton.icon(
-            onPressed: () =>
-                widget.appState.handleNavigationChange(Routes.startGame),
-            icon: const Icon(Icons.gamepad_outlined),
-            label: const Text("Neues Spiel starten"),
-          ),
-          ElevatedButton.icon(
-            onPressed: () =>
-                widget.appState.handleNavigationChange(Routes.openGames),
-            icon: const Icon(Icons.playlist_play_outlined),
-            label: const Text("Offene Spiele"),
-          ),
-        ],
-      ),
+        ),
+        // Padding(
+        //   padding: const EdgeInsets.all(10),
+        //   child: Text(
+        //     "Falsch als Fakt bezeichnet: " +
+        //         widget.appState.player!.falseCorrectAnswers.toString(),
+        //   ),
+        // ),
+        // Padding(
+        //   padding: const EdgeInsets.all(10),
+        //   child: Text(
+        //     "Falsch als Fake bezeichnet: " +
+        //         widget.appState.player!.falseFakeAnswers.toString(),
+        //   ),
+        // ),
+        // Padding(
+        //   padding: const EdgeInsets.all(10),
+        //   child: Text(
+        //     "Richtig als Fakt erkannt: " +
+        //         widget.appState.player!.trueCorrectAnswers.toString(),
+        //   ),
+        // ),
+        // Padding(
+        //   padding: const EdgeInsets.all(10),
+        //   child: Text(
+        //     "Richtig als Fake erkannt: " +
+        //         widget.appState.player!.trueFakeAnswers.toString(),
+        //   ),
+        // ),
+        // Padding(
+        //   padding: const EdgeInsets.all(10),
+        //   child: Text(
+        //     "Anzahl bewerteter Aussagen: " +
+        //         (widget.appState.player!.trueFakeAnswers +
+        //                 widget.appState.player!.trueCorrectAnswers +
+        //                 widget.appState.player!.falseCorrectAnswers +
+        //                 widget.appState.player!.falseFakeAnswers)
+        //             .toString(),
+        //   ),
+        // ),
+        ElevatedButton.icon(
+          onPressed: () =>
+              widget.appState.handleNavigationChange(Routes.startGame),
+          icon: const Icon(Icons.gamepad_outlined),
+          label: const Text("Neues Spiel starten"),
+        ),
+        ElevatedButton.icon(
+          onPressed: () =>
+              widget.appState.handleNavigationChange(Routes.openGames),
+          icon: const Icon(Icons.playlist_play_outlined),
+          label: const Text("Offene Spiele"),
+        ),
+      ],
     );
   }
 }
