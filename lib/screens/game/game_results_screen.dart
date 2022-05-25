48ea63b386a4b14f9e30c51fabfa3b25864e6e55
simonsasse <simonsasse97@gmail.com>
2022-05-25 17:48:05 +0200

    floating button for results screen

--- lib/screens/game/game_results_screen.dart
+++ lib/screens/game/game_results_screen.dart
@@ -26,65 +26,54 @@ class _GameResultsScreenState extends State<GameResultsScreen> {
       ),
       body: widget.appState.currentEnemy!.openGame!.statements == null
           ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: ListView.builder(
-                itemCount: widget.showAll
-                    ? widget
-                        .appState.currentEnemy!.openGame!.playerAnswers.length
-                    : 3,
-                itemBuilder: (BuildContext context, int index) {
-                  if (!widget.showAll) {
-                    // if not all shown, show last three in correct order
-                    index = widget.appState.currentEnemy!.openGame!
-                            .playerAnswers.length -
-                        3 +
-                        index;
-                  }
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      if (index == 0)
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20),
-                          child: Text(
-                            "Runde 1",
-                            style: Theme.of(context).textTheme.headline4,
-                          ),
-                        ),
-                      if (index == 3)
-                        Text(
-                          "Runde 2",
+          : ListView.builder(
+              itemCount: widget.showAll
+                  ? widget.appState.currentEnemy!.openGame!.playerAnswers.length
+                  : 3,
+              itemBuilder: (BuildContext context, int index) {
+                if (!widget.showAll) {
+                  // if not all shown, show last three in correct order
+                  index = widget.appState.currentEnemy!.openGame!.playerAnswers
+                          .length -
+                      3 +
+                      index;
+                }
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (index == 0)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Text(
+                          "Runde 1",
                           style: Theme.of(context).textTheme.headline4,
                         ),
-                      if (index == 6)
-                        Text(
-                          "Runde 3",
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                      StatementCard(
-                        statement: widget.appState.currentEnemy!.openGame!
-                            .statements!.statements[index],
-                        appState: widget.appState,
                       ),
-                    ],
-                  );
-                },
-              ),
+                    if (index == 3)
+                      Text(
+                        "Runde 2",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                    if (index == 6)
+                      Text(
+                        "Runde 3",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                    StatementCard(
+                      statement: widget.appState.currentEnemy!.openGame!
+                          .statements!.statements[index],
+                      appState: widget.appState,
+                    ),
+                  ],
+                );
+              },
             ),
-      bottomSheet: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding:
-                const EdgeInsets.only(bottom: 30, top: 10, left: 20, right: 20),
-            child: TextButton(
-              child: const Text("weiter"),
-              onPressed: () => widget.appState.route = Routes.gameReadyToStart,
-            ),
-          ),
-        ],
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text("weiter"),
+        icon: const Icon(Icons.forward),
+        onPressed: () => widget.appState.route = Routes.gameReadyToStart,
       ),
     );
   }
