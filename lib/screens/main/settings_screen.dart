46a5a9e27aebd5310c4d10f7096879e19e82a9e3
simonsasse <simonsasse97@gmail.com>
2022-06-22 13:24:51 +0200

    update button layout

--- lib/screens/main/settings_screen.dart
+++ lib/screens/main/settings_screen.dart
@@ -49,17 +49,75 @@ class _SettingsScreenState extends State<SettingsScreen> {
           child: Padding(
             padding: const EdgeInsets.all(15),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
                   height: 50,
                 ),
                 ValueListenableBuilder(
-                  valueListenable: emojiController,
+                  valueListenable: usernameController,
                   builder: (context, TextEditingValue value, __) {
                     return Row(
                       children: [
                         Expanded(
                           child: TextField(
+                            style: Theme.of(context).textTheme.bodyText2,
+                            inputFormatters: [
+                              UsernameTextFormatter(),
+                              FilteringTextInputFormatter.allow(
+                                  Utils.regexUsername),
+                            ],
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            controller: usernameController,
+                            autofillHints: const [AutofillHints.newUsername],
+                            decoration: const InputDecoration(
+                              hintText: "Gebe einen neuen Username ein.",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.transparent,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton.icon(
+                          onPressed: usernameController.text !=
+                                      widget.appState.player!.name &&
+                                  usernameController.text.length >=
+                                      Utils.usernameMinLength
+                              ? () {
+                                  HapticFeedback.selectionClick();
+                                  FocusManager.instance.primaryFocus?.unfocus();
+                                  widget.appState.player?.name =
+                                      usernameController.text;
+                                  widget.appState.updateUser();
+                                }
+                              : null,
+                          icon: const Icon(Icons.switch_access_shortcut),
+                          label: const Text("Username ändern."),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ValueListenableBuilder(
+                  valueListenable: emojiController,
+                  builder: (context, TextEditingValue value, __) {
+                    return Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            style: Theme.of(context).textTheme.bodyText2,
                             controller: emojiController,
                             inputFormatters: [
                               FilteringTextInputFormatter.allow(
@@ -68,9 +126,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
                             maxLength: 1,
                             decoration: const InputDecoration(
                               hintText: "Gebe einen neuen Emoji ein.",
-                              border: UnderlineInputBorder(
+                              counterText: "",
+                              border: OutlineInputBorder(
                                 borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
+                                  Radius.circular(20),
                                 ),
                               ),
                               filled: true,
@@ -101,55 +160,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     );
                   },
                 ),
-                ValueListenableBuilder(
-                  valueListenable: usernameController,
-                  builder: (context, TextEditingValue value, __) {
-                    return Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            inputFormatters: [
-                              UsernameTextFormatter(),
-                              FilteringTextInputFormatter.allow(
-                                  Utils.regexUsername),
-                            ],
-                            enableSuggestions: false,
-                            autocorrect: false,
-                            textCapitalization: TextCapitalization.none,
-                            controller: usernameController,
-                            autofillHints: const [AutofillHints.newUsername],
-                            decoration: const InputDecoration(
-                              hintText: "Gebe einen neuen Username ein.",
-                              border: UnderlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                              filled: true,
-                              fillColor: Colors.transparent,
-                              contentPadding: EdgeInsets.all(10),
-                            ),
-                          ),
-                        ),
-                        ElevatedButton.icon(
-                          onPressed: usernameController.text !=
-                                      widget.appState.player!.name &&
-                                  usernameController.text.length >=
-                                      Utils.usernameMinLength
-                              ? () {
-                                  HapticFeedback.selectionClick();
-                                  FocusManager.instance.primaryFocus?.unfocus();
-                                  widget.appState.player?.name =
-                                      usernameController.text;
-                                  widget.appState.updateUser();
-                                }
-                              : null,
-                          icon: const Icon(Icons.switch_access_shortcut),
-                          label: const Text("Username ändern."),
-                        ),
-                      ],
-                    );
-                  },
+                SizedBox(
+                  height: 20,
                 ),
                 Row(
                     mainAxisSize: MainAxisSize.max,
@@ -161,61 +173,72 @@ class _SettingsScreenState extends State<SettingsScreen> {
                           widget.appState.logout();
                         },
                         icon: const Icon(Icons.logout),
-                        label: const Text("Abmelden"),
+                        label: Text(
+                          "Abmelden",
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
                       ),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          HapticFeedback.heavyImpact();
-                          showModalBottomSheet(
-                              backgroundColor: Colors.transparent,
-                              context: context,
-                              builder: (BuildContext context) {
-                                return Container(
-                                  padding: const EdgeInsets.only(top: 50),
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.75,
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[800],
-                                    borderRadius: const BorderRadius.only(
-                                      topLeft: Radius.circular(15),
-                                      topRight: Radius.circular(15),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            HapticFeedback.heavyImpact();
+                            showModalBottomSheet(
+                                backgroundColor: Colors.transparent,
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Container(
+                                    padding: const EdgeInsets.only(top: 50),
+                                    height: MediaQuery.of(context).size.height *
+                                        0.75,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[800],
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(15),
+                                        topRight: Radius.circular(15),
+                                      ),
                                     ),
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      const Icon(
-                                        Icons.warning_amber_rounded,
-                                        color: DesignColors.red,
-                                        size: 200,
-                                      ),
-                                      Text(
-                                        "Willst du deinen Account wirklich für immer löschen?",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .subtitle1,
-                                      ),
-                                      ElevatedButton(
-                                        style: ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStateProperty.all<Color>(
-                                                  DesignColors.red),
+                                    child: Column(
+                                      children: [
+                                        const Icon(
+                                          Icons.warning_amber_rounded,
+                                          color: DesignColors.red,
+                                          size: 200,
                                         ),
-                                        onPressed: () {
-                                          widget.appState.db
-                                              .deleteAccount(widget.appState);
-                                        },
-                                        child: Text("Ich bin sicher."),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              });
-                        },
-                        icon: const Icon(Icons.delete_forever),
-                        label: const Text("Account löschen"),
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              DesignColors.red),
+                                        Text(
+                                          "Willst du deinen Account wirklich für immer löschen?",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle1,
+                                        ),
+                                        ElevatedButton(
+                                          style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all<
+                                                    Color>(DesignColors.red),
+                                          ),
+                                          onPressed: () {
+                                            widget.appState.db
+                                                .deleteAccount(widget.appState);
+                                          },
+                                          child: Text("Ich bin sicher."),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                });
+                          },
+                          icon: const Icon(Icons.delete_forever),
+                          label: Text(
+                            "Account löschen",
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                DesignColors.red),
+                          ),
                         ),
                       ),
                     ]),
@@ -239,13 +262,16 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ),
               ),
               Positioned(
-                top: 0,
+                top: -5,
                 child: RotationTransition(
                   turns: AlwaysStoppedAnimation(-8 / 360),
                   child: Center(
                     child: Text(
                       "Made in Berlin with ❤️ and ☕ and Steuergeld.",
-                      style: Theme.of(context).textTheme.bodyText2,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(color: DesignColors.backgroundBlue),
                     ),
                   ),
                 ),
@@ -278,7 +304,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
                               },
                               child: Text(
                                 "QuellenReiter.app",
-                                style: Theme.of(context).textTheme.bodyText1,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(color: Colors.white),
                               ),
                             ),
                             TextButton(
@@ -291,7 +320,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
                               },
                               child: Text(
                                 "Impressum",
-                                style: Theme.of(context).textTheme.bodyText1,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(color: DesignColors.lightGrey),
                               ),
                             ),
                             TextButton(
@@ -304,7 +336,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
                               },
                               child: Text(
                                 "Datenschutz",
-                                style: Theme.of(context).textTheme.bodyText1,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(color: DesignColors.lightGrey),
                               ),
                             ),
                           ],
