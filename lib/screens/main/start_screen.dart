8a67e10eadcf6010bee5a9bce25d7086e31a8393
simonsasse <simonsasse97@gmail.com>
2022-07-12 13:44:58 +0200

    change start screen

--- lib/screens/main/start_screen.dart
+++ lib/screens/main/start_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:quellenreiter_app/constants/constants.dart';
 import 'package:quellenreiter_app/models/quellenreiter_app_state.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 import '../../models/enemy.dart';
@@ -102,6 +103,76 @@ class _StartScreenState extends State<StartScreen> {
       for (Enemy e in widget.appState.player!.friends!.enemies) {
         if (e.openGame != null && e.openGame!.isPlayersTurn()) {
           playersTurn.add(EnemyCard(
+            appState: widget.appState,
+            enemy: e,
+            onTapped: (enemy) => {},
+          ));
+        }
+      }
+    }
+
+    // create "friendrequest" widgets
+    List<Widget> friendRequests = [];
+    // if any open game is finished and points are not accessed yet.
+    if (widget.appState.enemyRequests != null &&
+        widget.appState.enemyRequests!.enemies.isNotEmpty) {
+      // add the heading
+      friendRequests.add(
+        Padding(
+          padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.person_add,
+                color: DesignColors.backgroundBlue,
+                size: Theme.of(context).textTheme.headline2!.fontSize! + 10,
+              ),
+              Text(
+                "Neue Anfragen",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ],
+          ),
+        ),
+      );
+      for (Enemy e in widget.appState.enemyRequests!.enemies) {
+        friendRequests.add(EnemyCard(
+          appState: widget.appState,
+          enemy: e,
+          onTapped: (enemy) => {},
+        ));
+      }
+    }
+
+    // create "Start new game" widgets
+    List<Widget> startNewGame = [];
+    // if any open game is finished and points are not accessed yet.
+    if (widget.appState.player!.friends!.enemies
+        .any((element) => (element.openGame == null))) {
+      // add the heading
+      startNewGame.add(
+        Padding(
+          padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.sports_esports_outlined,
+                color: DesignColors.backgroundBlue,
+                size: Theme.of(context).textTheme.headline2!.fontSize! + 10,
+              ),
+              Text(
+                "Neues Spiel starten",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ],
+          ),
+        ),
+      );
+      for (Enemy e in widget.appState.player!.friends!.enemies) {
+        if (e.openGame == null) {
+          startNewGame.add(EnemyCard(
             appState: widget.appState,
             enemy: e,
             onTapped: (enemy) => {},
@@ -364,18 +435,46 @@ class _StartScreenState extends State<StartScreen> {
                     ),
                   ),
                 ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    HapticFeedback.mediumImpact();
-                    widget.appState.handleNavigationChange(Routes.startGame);
-                  },
-                  icon: Icon(
-                    Icons.sports_esports,
-                  ),
-                  label: Text(
-                    "Neues Spiel",
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              HapticFeedback.mediumImpact();
+                              widget.appState.route = Routes.addFriends;
+                            },
+                            icon: Icon(
+                              Icons.person_search_rounded,
+                            ),
+                            label: Text(
+                              "suchen",
+                              style: Theme.of(context).textTheme.headline1,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              HapticFeedback.mediumImpact();
+                              Share.share("https://quellenreiter.app",
+                                  subject:
+                                      "Teile die app mit deinen Freund:innen.");
+                            },
+                            icon: Icon(Icons.share_outlined),
+                            label: Text(
+                              "einladen",
+                              style: Theme.of(context).textTheme.headline1,
+                            ),
+                          ),
+                        ),
+                      ]),
                 ),
                 const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 15),
@@ -384,6 +483,7 @@ class _StartScreenState extends State<StartScreen> {
                   ),
                 ),
                 ...finishedGames,
+                ...friendRequests,
                 if (playersTurn.isEmpty && finishedGames.isEmpty)
                   Center(
                       child: Padding(
@@ -397,7 +497,8 @@ class _StartScreenState extends State<StartScreen> {
                             .copyWith(color: DesignColors.lightBlue)),
                   ))
                 else
-                  ...playersTurn
+                  ...playersTurn,
+                ...startNewGame
               ],
             ),
           ),
