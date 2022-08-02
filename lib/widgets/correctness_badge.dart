ce0bd59c8cd59d8b9fea67c3eea3df9cb8a387f9
simonsasse <simonsasse97@gmail.com>
2022-08-02 11:15:26 +0200

    redesign bewertungskategorien

--- lib/widgets/correctness_badge.dart
+++ lib/widgets/correctness_badge.dart
@@ -37,39 +37,41 @@ class CorrectnessBadge extends StatelessWidget {
             backgroundColor: Colors.transparent,
             isDismissible: true,
             builder: (BuildContext context) {
-              return Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
+              return SafeArea(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
                   ),
-                ),
-                child: SafeArea(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SelectableText(
-                        "Unsere Bewertungsskala",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline2!
-                            .copyWith(color: DesignColors.green),
-                      ),
-                      Flexible(
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: CorrectnessCategory.length(),
-                            itemBuilder: (context, index) {
-                              return getBadgeAndText(
-                                  CorrectnessCategory.at(index),
-                                  CorrectnessCategoryExplanation.at(index),
-                                  context);
-                            }),
-                      )
-                    ],
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SelectableText(
+                          "Unsere Bewertungsskala",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline2!
+                              .copyWith(color: DesignColors.green),
+                        ),
+                        Flexible(
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: CorrectnessCategory.length(),
+                              itemBuilder: (context, index) {
+                                return getBadgeAndText(
+                                    CorrectnessCategory.at(index),
+                                    CorrectnessCategoryExplanation.at(index),
+                                    context);
+                              }),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               );
@@ -82,32 +84,47 @@ class CorrectnessBadge extends StatelessWidget {
 
   Widget getBadgeAndText(
       String badgeText, String mainText, BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: Container(
-            margin: const EdgeInsets.only(top: 10, bottom: 10),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-              color: badgeText == CorrectnessCategory.correct
-                  ? DesignColors.green
-                  : DesignColors.red,
-            ),
-            child: SelectableText(
-              badgeText,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Container(
+        alignment: Alignment.topLeft,
+        clipBehavior: Clip.none,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: DesignColors.lightGrey,
         ),
-        Flexible(
-          child: SelectableText(
-            mainText,
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-        )
-      ],
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  color: badgeText == CorrectnessCategory.correct
+                      ? DesignColors.green
+                      : DesignColors.red,
+                ),
+                child: SelectableText(
+                  badgeText,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ),
+            ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: SelectableText(
+                  mainText,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
